a7ba759613f0786ad015ffd3521d3bf382118434
Anima-t3d <wings_of_apollo@belgacom.net>
2019-09-08 22:19:09 +0800

    Update answer to return fizz

--- __tests__/index.js
+++ __tests__/index.js
@@ -7,5 +7,10 @@ describe('Code kata', () => {
 		expect(answer(4)).toEqual(4);
 		expect(answer(7)).toEqual(7);
 		expect(answer(8)).toEqual(8);
+  });
+  it('should return fizz', () => {
+		expect(answer(3)).toEqual('fizz');
+		expect(answer(6)).toEqual('fizz');
+		expect(answer(9)).toEqual('fizz');
 	});
 });
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const answer = (number) => {
+  if (number % 3 == 0) {
+    return 'fizz';
+  }
 	return number;
 }
 export default answer;
